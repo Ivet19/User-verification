23baf08a4a6df7f1cc73ff50d00b88b8e26f1717
Ivet Lopez <ivet19@live.com>
2025-02-08 22:20:57 +0100

    Corección de magic numbers

--- js/R1-user-validation.js
+++ js/R1-user-validation.js
@@ -1,16 +1,19 @@
 const username = "ivet19";
-const isValidUsername = username.length >= 5;
+const minimumUsernameLength = 5;
+const isValidUsername = username.length >= minimumUsernameLength;
 
 const password = "Bootcamp2025";
 const repeatPassword = "Bootcamp2025";
 const isRepeatPassword = repeatPassword === password;
+const minimumPasswordLength = 8;
 const isValidPassword =
-  password.length >= 8 &&
+  password.length >= minimumPasswordLength &&
   /\d/.test(password) &&
   /[A-ZÀÈÌÒÙÁÉÍÓÚÑ]/.test(password);
 
 let age = 27;
-const isAdult = age >= 18;
+const minimumAdultAge = 18;
+const isAdult = age >= minimumAdultAge;
 
 console.log(`El nombre de usuario es válido: ${isValidUsername}
 La contraseña es válida: ${isValidPassword}
