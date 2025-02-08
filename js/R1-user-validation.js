e3441e785c087a73be5cf919f4906781e14b83e9
Ivet Lopez <ivet19@live.com>
2025-02-08 19:32:24 +0100

    Corrección requisito mayúsculas con acento y letra Ñ

--- js/R1-user-validation.js
+++ js/R1-user-validation.js
@@ -1,17 +1,19 @@
-const userName = "ivet19";
-const isUserNameValid = userName.length >= 5;
+const username = "ivet19";
+const isValidUsername = username.length >= 5;
 
 const password = "Bootcamp2025";
-const passwordRepeat = "Bootcamp2025";
-const isPasswordRepeat = passwordRepeat === password;
-const isPasswordValid =
-  password.length >= 8 && /\d/.test(password) && /[A-Z]/.test(password);
+const repeatPassword = "Bootcamp2025";
+const isRepeatPassword = repeatPassword === password;
+const isValidPassword =
+  password.length >= 8 &&
+  /\d/.test(password) &&
+  /[A-ZÀÈÌÒÙÁÉÍÓÚÑ]/.test(password);
 
 let age = 27;
 const isAdult = age >= 18;
 
-console.log(`El nombre de usuario es válido: ${isUserNameValid}
-La contraseña es válida: ${isPasswordValid}
-La repetición de la contraseña es válida: ${isPasswordRepeat}
+console.log(`El nombre de usuario es válido: ${isValidUsername}
+La contraseña es válida: ${isValidPassword}
+La repetición de la contraseña es válida: ${isRepeatPassword}
 Es mayor de edad: ${isAdult}
   `);
